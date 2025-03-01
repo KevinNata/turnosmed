69be72bf4ede1c74ccad637ed3389ec508351182
kevinnatalini <kevinrugby333@gmail.com>
2025-03-01 14:52:21 +0100

    Agrego funciones de insert y delete a editarmedico

--- obj/Debug/net6.0/TurnosMedicos.AssemblyInfo.cs
+++ obj/Debug/net6.0/TurnosMedicos.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TurnosMedicos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+642bdbf20925bb1b083f0855c5e022b0cb7f0ae3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdbc28f4d5cc8fdfd253b1ca0d2f15f7f6ae3539")]
 [assembly: System.Reflection.AssemblyProductAttribute("TurnosMedicos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TurnosMedicos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
